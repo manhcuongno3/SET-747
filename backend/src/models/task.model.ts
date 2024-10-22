1555a17ff0fe473d2993ab08c67fbe324e41cd7d
CuongPC <manhcuongno3@gmail.com>
2024-10-23 06:25:30 +0700

    Complete integrate mongo, handle crud task

---
+++ backend/src/models/task.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Task extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  taskName: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  isChecked: boolean;
+
+
+  constructor(data?: Partial<Task>) {
+    super(data);
+  }
+}
+
+export interface TaskRelations {
+  // describe navigational properties here
+}
+
+export type TaskWithRelations = Task & TaskRelations;
